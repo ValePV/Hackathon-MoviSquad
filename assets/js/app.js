dbc753bf7cc82b6a96df1203fb8e2ef44a503b17
valePV <va.perez19@hotmail.com>
2018-03-31 14:57:49 -0300

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,24 +30,7 @@ $(document).ready(function() {
     $('.btn_movie').show();
   });
 
-  $('#user-photo').click(function() {
-    $('#photo').show();
-    $('.first_section').hide();
-    $('.second-section').hide();
-    $('.third_section').hide();
-    $('.fourth_section').hide();
-    $('.btn_movie').hide();
-  });
-
-  $('.user-navb').click(function() {
-    $('#photo').show();
-    $('.first_section').hide();
-    $('.second-section').hide();
-    $('.third_section').hide();
-    $('.fourth_section').hide();
-    $('.btn_movie').hide();
-  });
-
+  
   $('#bridge1').click(function() {
     $('.first_section').hide();
     $('.second-section').hide();
@@ -161,15 +144,15 @@ $(document).ready(function() {
 
 // FIREBASE
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBosbsodzYp0HFlVLfHKfMJWMM_4WQYttE",
-    authDomain: "usuario-90e05.firebaseapp.com",
-    databaseURL: "https://usuario-90e05.firebaseio.com",
-    projectId: "usuario-90e05",
-    storageBucket: "usuario-90e05.appspot.com",
-    messagingSenderId: "431818765808"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyBosbsodzYp0HFlVLfHKfMJWMM_4WQYttE",
+  authDomain: "usuario-90e05.firebaseapp.com",
+  databaseURL: "https://usuario-90e05.firebaseio.com",
+  projectId: "usuario-90e05",
+  storageBucket: "usuario-90e05.appspot.com",
+  messagingSenderId: "431818765808"
+};
+firebase.initializeApp(config);
 
 var loginGoogle = document.getElementById('btn-google');
 var loginFacebook = document.getElementById('btn-facebook');
@@ -267,8 +250,10 @@ function InicializarFire() {
       $('.user-navb').append('<img class="user-nav" src ='+userPhoto+' >');
       $('.btn-login').addClass('hidden')
       $('.close-session').removeClass('hidden')
-      $('.user-info').append('<img class="user-information" src ='+userPhoto+' >')
-      $('.info-user').append('<h2>' + displayName + '</h2><h4>' + email + '</h4>')
+      $('.cont-myperfil').removeClass('hidden')
+      $('.user-info').append('<img class="user-information img-responsive" src ='+userPhoto+' >')
+      $('.info-user').append('<h2>' + displayName + '</h2>'
+        + '<h4>' + email + '</h4>')
       // document.querySelector('span.user-name').textContent= displayName;
      // userName.textContent = displayName;
      // if(userPhoto) {
@@ -284,6 +269,7 @@ function InicializarFire() {
       firebase.auth().signOut();
       $('.user-navb').empty();
     }
+
   });
 }
 
@@ -319,6 +305,7 @@ window.onload = function() {
 $(document).ready(function() {
   $('#search-form').on('submit', function(e) {
     $('#contmovies').empty();
+    $('#show').empty();
     $('.first_section').hide();
     $('.second-section').show();
     let movie = ($('#search-movie').val());
@@ -375,22 +362,28 @@ function getMovies(movie) {
       });
     }
   }
+
+  var infoFav = firebase.database().ref('Users');
+    
   function showMovie(info) {
     if (info.Response === 'false') {
       alert('Pelicula no encontrada');
     } else {
       console.log(info.Title);
-      console.log("yellow");
+      
       $('.movie-section').hide();
       $('.third_section').show();
       $('#showmovies, #contmovies, #title, #year, #plot, #actors, #awards, #recaudacion, #country, #director, #genre, #language, #production, #metascore, #imdbRating, #released, #writer, #runtime, #img, #trailer, #results').empty();
-
+      
+      var user = firebase.auth().currentUser;
+      console.log(user);
+      
+      if(!user){
         $('#show').append(`
           <div class='row'>
             <div class="col-md-3 interaction">
               <img src="${info.Poster}">
               <button id="trailer-button" class="btn btn-default btn-trailer" type="button" name="button">Trailer</button>
-              <button class="btn btn-default" type="button" name="button">hnjbhj</button>
             </div>
             <div class="col-md-7 col-md-offset-1">
               <h5>Title:</h5> <h3>${info.Title}</h3>
@@ -416,9 +409,45 @@ function getMovies(movie) {
             </div>
           </div>
       `);
-      
-             
-         
+      } else {
+        $('#show').append(`
+          <div class='row'>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 interaction">
+              <img src="${info.Poster}">
+              <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                  <button id="trailer-button" class="btn btn-default btn-trailer" type="button" name="button">Trailer</button>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                  <button class="btn btn-default btn-fav" type="button" name="button">Add Favourite</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-7 col-md-offset-1 col-lg-7 col-lg-offset-1">
+              <h5>Title:</h5> <h3>${info.Title}</h3>
+              <h5>Argumento:</h5> <h4>${info.Plot}</h4>
+              <h5>Director:</h5> <h4>${info.Director}</h4>
+              <h5 class='cont-afterhide'>Producción:</h5> <h4 class='cont-afterhide'>${info.Production}</h4>
+              <h5 class='cont-afterhide'>Guión:</h5> <h4 class='cont-afterhide'>${info.Writer}</h4>
+              <h5>Protagonistas:</h5> <h4>${info.Actors}</h4>
+              <h5 class='cont-afterhide'>Premios:</h5> <h4 class='cont-afterhide'>${info.Awards}</h4>
+              <h5 class='cont-afterhide'>Pais:</h5> <h4>${info.Country}</h4>
+              <h5 class='cont-afterhide'>Idioma:</h5> <h4 class='cont-afterhide'>${info.Language}</h4>
+              <h5 class='cont-afterhide'>Año:</h5> <h4 class='cont-afterhide'>${info.Year}</h4>
+              <h5 class='cont-afterhide'>Estreno:</h5> <h4 class='cont-afterhide'>${info.Released}</h4>
+              <h5 class='cont-afterhide'>Género:</h5> <h4 class='cont-afterhide'>${info.Genre}</h4>
+              <h5 class='cont-afterhide'>Duración:</h5> <h4 class='cont-afterhide'>${info.Runtime}</h4>
+              <h5 class='cont-afterhide'>Recaudacion:</h5> <h4 class='cont-afterhide'>${info.BoxOffice}</h4>
+              <h5 class='cont-afterhide'>Crítica:</h5> <h4 class='cont-afterhide'>${info.Metascore}</h4>
+              <h5 class='cont-afterhide'>Nota del Público:</h5> <h4 class='cont-afterhide'>${info.imdbRating}</h4>
+            </div>
+          </div>  
+          <div class='row'>
+            <div id='results'>
+            </div>
+          </div>
+      `);
+      }  
       /*
       $('#title').append(`Titulo: ${info.Title}`);
       $('#year').append(`Año: ${info.Year}`);+
@@ -438,7 +467,21 @@ function getMovies(movie) {
       $('#runtime').append(`Duración: ${info.Runtime}`);
       $('#img').append(`<img src='${info.Poster}'>`);
       $('#trailer').append('<button id="trailer-button" type="button" name="button">trailer</button>');*/
+      
     };
+
+    $('.btn-fav').click(function(){
+      infoFav.push({
+        userID:user.email,
+        Imagen : info.Poster,
+        Title : info.Title
+             
+      })
+      console.log(infoFav);
+    })
+      
+
+
 
     $('#trailer-button').click(function() {
       let apikey = 'AIzaSyA9Yzrb5VubC6kzQnqCaUMSUd-QiRCtamI';
@@ -463,7 +506,27 @@ function getMovies(movie) {
         });
     });
   }
+
+  $('.my-perfil').click(function() {
+    $('#photo').show();
+    $('.first_section').hide();
+    $('.second-section').hide();
+    $('.third_section').hide();
+    $('.fourth_section').hide();
+    $('.btn_movie').hide();
+
+    infoFav.orderByChild('userID').on('value', function(snapshot){
+      snapshot.forEach(function(e){
+        var info = e.val();
+        $('.user-search').append('<div class="title-movie thumbnail card"><img class="img-mov img-responsive" src="' 
+          + info.Imagen + '"><p class="txt_movie">' + info.Title + '</div>');
+      })
+    })
+   
+  });
   
+
+
   function getOutput(item) {
     console.log(item);
     var videoID = item.id.videoId;
